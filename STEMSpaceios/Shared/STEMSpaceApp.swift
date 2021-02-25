4c4b7c712f913ce515fae10aad3809bb0277d676
IDGitHubMan <40811090+IDGitHubMan@users.noreply.github.com>
2021-02-25 09:57:01 -0500

    Create basic android and mac projects for the app. Be done with  python.

---
+++ STEMSpaceios/Shared/STEMSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMSpaceApp.swift
+//  Shared
+//
+//  Created by Isaiah Desrosiers on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct STEMSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
